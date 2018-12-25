b53feb20ea4f1ba950c23b7aa69d9b9c34dd3fdf
Alexey Dulayev <dulayev@gmail.com>
2018-12-25 23:23:27 +0300

    Using MediaRedorder and work on permissions

---
+++ Dashcam/app/src/main/java/com/dulayev/dashcam/MainActivity.java
@@ -0,0 +1,136 @@
+package com.dulayev.dashcam;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.media.CamcorderProfile;
+import android.media.MediaRecorder;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class MainActivity extends Activity implements View.OnClickListener, SurfaceHolder.Callback {
+    MediaRecorder recorder;
+    SurfaceHolder holder;
+    boolean recording = false;
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        recorder = new MediaRecorder();
+        List<String> permissions = Arrays.asList(
+                Manifest.permission.RECORD_AUDIO,
+                Manifest.permission.CAMERA,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        boolean granted = permissions.stream().allMatch(
+                p -> ContextCompat.checkSelfPermission(this, p) == PackageManager.PERMISSION_GRANTED);
+
+        if (!granted) {
+            ActivityCompat.requestPermissions(this,  permissions.stream().toArray(String[]::new), 0);
+        }
+
+        setContentView(R.layout.main);
+
+        SurfaceView cameraView = (SurfaceView) findViewById(R.id.CameraView);
+        holder = cameraView.getHolder();
+        holder.addCallback(this);
+        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        cameraView.setClickable(true);
+        cameraView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        // If request is cancelled, the result arrays are empty.
+        if (grantResults.length > 0
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            // permission was granted, yay! Do the
+            // contacts-related task you need to do.
+            initRecorder();
+
+        } else {
+            // permission denied, boo! Disable the
+            // functionality that depends on this permission.
+        }
+    }
+
+
+    private void initRecorder() {
+        recorder.setAudioSource(MediaRecorder.AudioSource.DEFAULT);
+        recorder.setVideoSource(MediaRecorder.VideoSource.DEFAULT);
+
+        CamcorderProfile cpHigh = CamcorderProfile
+                .get(CamcorderProfile.QUALITY_HIGH);
+        recorder.setProfile(cpHigh);
+        recorder.setOutputFile("/sdcard/videocapture_example.mp4");
+        recorder.setMaxDuration(50000); // 50 seconds
+        recorder.setMaxFileSize(5000000); // Approximately 5 megabytes
+    }
+
+    private void prepareRecorder() {
+        recorder.setPreviewDisplay(holder.getSurface());
+
+        try {
+            recorder.prepare();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+            finish();
+        } catch (IOException e) {
+            e.printStackTrace();
+            finish();
+        }
+    }
+
+    public void onClick(View v) {
+        if (recording) {
+            recorder.stop();
+            recording = false;
+
+            // Let's initRecorder so we can record again
+            initRecorder();
+            prepareRecorder();
+        } else {
+            recording = true;
+            recorder.start();
+        }
+    }
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        prepareRecorder();
+    }
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int width,
+                               int height) {
+    }
+
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        if (recording) {
+            recorder.stop();
+            recording = false;
+        }
+        recorder.release();
+        finish();
+    }
+}
