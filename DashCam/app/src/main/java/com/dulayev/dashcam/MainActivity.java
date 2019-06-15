706d6789dc826f05962735cacba3981d7c17905d
dulayev <dulayev@gmail.com>
2019-06-15 23:59:19 +0300

    Move video recording into Service

--- DashCam/app/src/main/java/com/dulayev/dashcam/MainActivity.java
+++ DashCam/app/src/main/java/com/dulayev/dashcam/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCaptureSession;
 import android.hardware.camera2.CameraCharacteristics;
@@ -18,6 +20,8 @@ import android.os.Environment;
 import android.os.Handler;
 import android.util.Log;
 import android.view.Surface;
+import android.view.View;
+import android.widget.Button;
 
 import java.io.File;
 import java.io.IOException;
@@ -28,12 +32,6 @@ import java.util.TimerTask;
 public class MainActivity extends AppCompatActivity {
 
     final String TAG = "DashCam";
-    private CameraDevice camera;
-    private MediaRecorder recorder;
-    private Surface surface;
-    private CaptureRequest.Builder builder;
-    private CameraCaptureSession session;
-    private Timer timer;
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
@@ -42,125 +40,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate");
 
-        CameraManager cameraManager = getSystemService(CameraManager.class);
-        String cameraId = null;
-        try {
-            String[] idList = cameraManager.getCameraIdList();
-
-            for (String id : idList) {
-                CameraCharacteristics characteristics = cameraManager.getCameraCharacteristics(id);
-                if (characteristics.get(CameraCharacteristics.LENS_FACING).equals(CameraMetadata.LENS_FACING_BACK)) {
-                    cameraId = id;
-                    Log.d(TAG, "Back camera is: " + id);
-                    break;
-                }
-            }
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
-        Log.d(TAG, "Camera selected: " + cameraId);
-
-        if (cameraId != null) {
-            final CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
-                @Override
-                public void onOpened(@androidx.annotation.NonNull CameraDevice camera) {
-                    Log.d(TAG, "onOpened");
-                        OnCameraOpened(camera);
-                }
-
-                @Override
-                public void onDisconnected(@androidx.annotation.NonNull CameraDevice camera) {
-                    Log.d(TAG, "onDisconnected");
-                }
-
-                @Override
-                public void onError(@androidx.annotation.NonNull CameraDevice camera, int error) {
-                    Log.d(TAG, "onError");
-                }
-            };
-            try {
-                cameraManager.openCamera(cameraId, stateCallback, null/*current thread looper*/);
-            } catch (CameraAccessException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    private void OnCameraOpened(CameraDevice camera) {
-        this.camera = camera;
-        try {
-            this.recorder = new MediaRecorder();
-            recorder.setVideoSource(MediaRecorder.VideoSource.SURFACE);
-            recorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
-            recorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
-            File picturesDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
-            recorder.setOutputFile(picturesDir.getPath() + "/dc.mp4");
-            recorder.setVideoSize(1920, 1080);
-            recorder.setVideoFrameRate(30);
-            try {
-                recorder.prepare();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            //recorder.start();
-            this.surface = recorder.getSurface();
-
-            this.builder = camera.createCaptureRequest(CameraDevice.TEMPLATE_RECORD);
-            builder.addTarget(surface);
-
-
-            camera.createCaptureSession(Arrays.asList(surface), new CameraCaptureSession.StateCallback() {
-                @Override
-                public void onConfigured(@NonNull CameraCaptureSession session) {
-                    Log.d(TAG, "onConfigured");
-                    OnCaptureSessionConfigured(session);
-                }
-
-                @Override
-                public void onConfigureFailed(@NonNull CameraCaptureSession session) {
-                    Log.d(TAG, "onConfigureFailed");
-                }
-            }, null);
-
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void OnCaptureSessionConfigured(CameraCaptureSession session) {
-        this.session = session;
-        recorder.start();
-        try {
-            session.setRepeatingRequest(builder.build(), new CameraCaptureSession.CaptureCallback() {
-                @Override
-                public void onCaptureStarted(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, long timestamp, long frameNumber) {
-                    super.onCaptureStarted(session, request, timestamp, frameNumber);
-                }
-            }, null);
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
-        this.timer = new Timer();
-        final Handler handler = new Handler();
-        timer.schedule(new TimerTask() {
+        Button playButton = (Button)findViewById(R.id.playButton);
+        playButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        OnTimerEnd();
-                    }
-                });
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                Intent intent = new Intent(context, RecorderService.class);
+                //i.putExtra("KEY1", "Value to be used by the service");
+                context.startService(intent);
             }
-        }, 6000/*ms*/);
+        });
+
     }
 
-    private void OnTimerEnd() {
-        try {
-            this.session.stopRepeating();
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
-        recorder.stop();
-    }
 }
---
+++ DashCam/app/src/main/java/com/dulayev/dashcam/RecorderService.java
@@ -0,0 +1,179 @@
+package com.dulayev.dashcam;
+
+import android.app.Service;
+import android.content.Intent;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CameraMetadata;
+import android.hardware.camera2.CaptureRequest;
+import android.media.MediaRecorder;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.Surface;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class RecorderService extends Service {
+
+    final String TAG = getClass().getSimpleName();
+    private CameraDevice camera;
+    private MediaRecorder recorder;
+    private Surface surface;
+    private CaptureRequest.Builder builder;
+    private CameraCaptureSession session;
+    private Timer timer;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        Log.d(TAG, "onStart");
+
+        CameraManager cameraManager = getSystemService(CameraManager.class);
+        String cameraId = null;
+        try {
+            String[] idList = cameraManager.getCameraIdList();
+
+            for (String id : idList) {
+                CameraCharacteristics characteristics = cameraManager.getCameraCharacteristics(id);
+                if (characteristics.get(CameraCharacteristics.LENS_FACING).equals(CameraMetadata.LENS_FACING_BACK)) {
+                    cameraId = id;
+                    Log.d(TAG, "Back camera is: " + id);
+                    break;
+                }
+            }
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+        Log.d(TAG, "Camera selected: " + cameraId);
+
+        if (cameraId != null) {
+            final CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
+                @Override
+                public void onOpened(@NonNull CameraDevice camera) {
+                    Log.d(TAG, "onOpened");
+                    OnCameraOpened(camera);
+                }
+
+                @Override
+                public void onDisconnected(@NonNull CameraDevice camera) {
+                    Log.d(TAG, "onDisconnected");
+                }
+
+                @Override
+                public void onError(@NonNull CameraDevice camera, int error) {
+                    Log.d(TAG, "onError");
+                }
+            };
+            try {
+                cameraManager.openCamera(cameraId, stateCallback, null/*current thread looper*/);
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        return Service.START_NOT_STICKY;
+    }
+
+    private void OnCameraOpened(CameraDevice camera) {
+        this.camera = camera;
+        try {
+            this.recorder = new MediaRecorder();
+            recorder.setVideoSource(MediaRecorder.VideoSource.SURFACE);
+            recorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+            recorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
+            File picturesDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
+            recorder.setOutputFile(picturesDir.getPath() + "/dc.mp4");
+            recorder.setVideoSize(1920, 1080);
+            recorder.setVideoFrameRate(30);
+            try {
+                recorder.prepare();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            //recorder.start();
+            this.surface = recorder.getSurface();
+
+            this.builder = camera.createCaptureRequest(CameraDevice.TEMPLATE_RECORD);
+            builder.addTarget(surface);
+
+
+            camera.createCaptureSession(Arrays.asList(surface), new CameraCaptureSession.StateCallback() {
+                @Override
+                public void onConfigured(@NonNull CameraCaptureSession session) {
+                    Log.d(TAG, "onConfigured");
+                    OnCaptureSessionConfigured(session);
+                }
+
+                @Override
+                public void onConfigureFailed(@NonNull CameraCaptureSession session) {
+                    Log.d(TAG, "onConfigureFailed");
+                }
+            }, null);
+
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void OnCaptureSessionConfigured(CameraCaptureSession session) {
+        this.session = session;
+        recorder.start();
+        try {
+            session.setRepeatingRequest(builder.build(), new CameraCaptureSession.CaptureCallback() {
+                @Override
+                public void onCaptureStarted(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, long timestamp, long frameNumber) {
+                    super.onCaptureStarted(session, request, timestamp, frameNumber);
+                }
+            }, null);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+        this.timer = new Timer();
+        final Handler handler = new Handler();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        OnTimerEnd();
+                    }
+                });
+            }
+        }, 6000/*ms*/);
+    }
+
+    private void OnTimerEnd() {
+        try {
+            this.session.stopRepeating();
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+        recorder.stop();
+    }
+
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
